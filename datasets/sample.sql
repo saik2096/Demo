d921e1578ff36095fdbed3950fafbf2e09058bcb
saik2096 <masanam.saikumar@gmail.com>
2023-01-31 12:18:24 +0530

    new obj

--- datasets/sample.sql
+++ datasets/sample.sql
@@ -7,4 +7,5 @@ CREATE TABLE crick__c (
 );
 INSERT INTO "crick__c" VALUES(1,'huui','8989');
 INSERT INTO "crick__c" VALUES(2,'lallq','4455');
+INSERT INTO "crick__c" VALUES(3,'lallqnjdoiujAO','899899');
 COMMIT;
