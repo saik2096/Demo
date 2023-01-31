18bee92a0a069e27ec5deeccff266ff83f0f69aa
saik2096 <masanam.saikumar@gmail.com>
2023-01-31 12:10:50 +0530

    objects

---
+++ datasets/sample.sql
@@ -0,0 +1,10 @@
+BEGIN TRANSACTION;
+CREATE TABLE crick__c (
+	id INTEGER NOT NULL, 
+	"Name" VARCHAR(255), 
+	"Number__c" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "crick__c" VALUES(1,'huui','8989');
+INSERT INTO "crick__c" VALUES(2,'lallq','4455');
+COMMIT;
